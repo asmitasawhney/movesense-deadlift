bf0d434cf5bc39a0338c723b1793895a1970277c
Asmita Sawhney <asmitasawhney@gmail.com>
2023-04-18 23:32:54 -0400

    wip

--- myApp/interface.cpp
+++ myApp/interface.cpp
@@ -115,7 +115,7 @@ void myApp::processData(wb::ResourceId resourceId, const wb::Value &value){
   uint8_t tag = 5;
 
   bool flag = true;
-  if (x_avg > 3.0f and y_avg > 3.0f and flag) {
+  if (x_avg > 2.0f and y_avg > 2.0f and flag) {
   	sendPacket((uint8_t *)bad, sizeof(bad), tag, Responses::COMMAND_RESULT);
   //	ledSetPattern(1000,2000,1);
   	flag = false;
